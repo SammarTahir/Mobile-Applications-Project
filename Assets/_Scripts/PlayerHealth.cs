2ce54f4041f56f3751dd5be99406318ee56b200d
SammarTahir <G00347526@gmit.ie>
2019-11-07 14:18:54 +0000

    Player health

---
+++ Assets/_Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int health;
+	public bool dead;
+
+	// Use this for initialization
+	void Start () {
+		dead = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < -6){
+			dead = true;
+		}
+		if (dead == true) {
+			StartCoroutine ("Die");
+		}
+	}
+
+	IEnumerator Die(){
+		SceneManager.LoadScene("Level1");
+		yield return null;
+		
+	}
+
+}
